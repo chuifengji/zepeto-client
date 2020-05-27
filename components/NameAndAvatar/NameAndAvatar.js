4b1f4991bb74b2b1c5a0410004759ea29bc01893
ntgeek <2584172930@qq.com>
2020-05-27 17:34:22 +0800

    writing style for index page

---
+++ components/NameAndAvatar/NameAndAvatar.js
@@ -0,0 +1,28 @@
+// components/NameAndAvatar/NameAndAvatar.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    userName: {
+      type: String,
+      value: ''
+    },
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    gotofriend_page: function () {
+      this.triggerEvent('gotofriend_page');
+    },
+  }
+})
